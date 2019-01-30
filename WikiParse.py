a4033e4e49a4761471b558143c22df02e0e952f1
Petya <petyo333@gmail.com>
2019-01-30 19:04:19 +0300

    add the ability to remove titles

--- WikiParse.py
+++ WikiParse.py
@@ -1,13 +1,22 @@
 import json
 import wikipediaapi
 
-def parse_page(w):
-    return len(w.text)
-
 articles = open('articles.txt', encoding='utf-8').read().splitlines()
+bad_section_titles = set(open('bad_section_titles', encoding='utf-8').read().splitlines())
 langs = open('langs.txt', encoding='utf-8').read().splitlines()
 res = {lang:([-100] * len(articles)) for lang in langs}
 wiki = wikipediaapi.Wikipedia('en')
+
+def parse_sections(p):
+    res = ''
+    for s in p.sections:
+        if s.title not in bad_section_titles:
+            res += s.title + '\n' + s.text + '\n' + parse_sections(s)
+    return res
+
+def parse_page(p):
+    return len(parse_sections(p))
+    
 for ind, article in enumerate(articles):
     page = wiki.page(article)
     langlinks = page.langlinks
