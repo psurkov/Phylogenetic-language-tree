00d57ac1f7a09e75fabe774cfaae8eea04507f58
Petya <petyo333@gmail.com>
2019-01-31 16:42:32 +0300

    refactoring

--- Drevostroy.py
+++ Drevostroy.py
@@ -1,76 +1,60 @@
 from ete3 import Tree, TreeStyle
 from math import sqrt
 
-f = open('WikiData.txt', 'r')
+f = open('data2.txt', 'r')
 s = []
 tree = Tree()
 name2node = {}
-for i in f:
-    a = ''
-    k = 0
-    b = []
-    while str(i[k]) != ' ':
-        a += str(i[k])
-        k += 1
-    name2node[a] = tree.add_child(name = a)
-    b.append(a)
-    while k < len(i) - 1:
-        c = ''
-        while str(i[k]) != ' ' and k < len(i) - 1:
-            c += str(i[k])
-            k += 1
-        if c != '':
-            b.append(int(c))
-        if str(i[k]) == ' ':
-            k += 1
-    s.append(b)
+for line in f:
+    lang = line.split()[0]
+    name2node[lang] = tree.add_child(name = lang)
+    s.append([lang] + [float(i) for i in line.split()[1:-1]])
+    
+def lang_dist(a, b):
+    dst = 0
+    for i in range(1, len(a)):
+        dst += (a[i] - b[i]) ** 2
+    dst = dst ** 0.5
+    return dst
+
+def lang_abs(a):
+    dst = 0
+    for i in range(1, len(a)):
+        dst += a[i] ** 2
+    dst = dst ** 0.5
+    return dst
+    
 def rang():
     global s
-    r = []
-    print(s)
-    for i in s:
-        b = [i[0]]
-        for h in s:
-            k = 1
-            summ = 0
-            while k < len(i):
-                summ += ((i[k] - h[k]) * (i[k] - h[k]))
-                k += 1
-            b.append((summ))
-        r.append(b)
-    y = 0
-    ty = 0
-    tx = 2
-    t = r[ty][tx]
-    for y, line in enumerate(r):
-        for x, dot in enumerate(line):
-            if x !=0 and dot !=0 and dot <= t:
-                t = dot
-                tx = x - 1
-                ty = y
-    q = 1
-    a = ['(' + r[tx][0] + '+' + r[ty][0] + ')']
-    wtx = r[tx][0].count('+') + 1
-    wty = r[ty][0].count('+') + 1
-    while q < len(s[tx]):
-        a.append((s[tx][q] * wtx + s[ty][q] * wty) / (wtx + wty))
-        q += 1
-    s.append(a)
+    x, y, best_dist = 0, 1, lang_dist(s[0], s[1])
+    for i in range(len(s)):
+        for j in range(i + 1, len(s)):
+            d = lang_dist(s[i], s[j])
+            if d < best_dist:
+                x = i
+                y = j
+                best_dist = d
+            print(s[i][0], s[j][0], d)
+    
+    a = ['(' + s[x][0] + '+' + s[y][0] + ')']
+    wx = s[x][0].count('+') + 1
+    wy = s[y][0].count('+') + 1
+    for i in range(1, len(s[x])):
+        a.append((s[x][i] * wx + s[y][i] * wy) / (wx + wy))
     
     new_name = a[0]
-    print(a[0])
     new_node = tree.add_child(name = new_name)
     name2node[new_name] = new_node
-    name2node[r[tx][0]].detach()
-    name2node[r[ty][0]].detach()
-    new_node.add_child(name2node[r[tx][0]], dist = t)
-    new_node.add_child(name2node[r[ty][0]], dist = t)
+    name2node[s[x][0]].detach()
+    name2node[s[y][0]].detach()
+    new_node.add_child(name2node[s[x][0]], dist = best_dist)
+    new_node.add_child(name2node[s[y][0]], dist = best_dist)
     
-    s.pop(tx)
-    s.pop(ty - 1)
+    s.pop(x)
+    s.pop(y - 1)
+    s.append(a)
+    
 while len(s) > 1:
     rang()
-#edge_labels=networkx.draw_networkx_edge_labels(g,pos=networkx.spring_layout(g),edge_labels=None,label_pos=1,clip_on=True)
-def pavuk():
-	tree.show()
-pavuk()
+
+tree.show()
