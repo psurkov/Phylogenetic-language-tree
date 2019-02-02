9541dddcfde8d9aa40cff495fbd65817ce9f2ff7
Petya <petyo333@gmail.com>
2019-02-02 12:35:17 +0300

    Update method

---
+++ BuildTree.py
@@ -0,0 +1,37 @@
+import networkx
+import matplotlib.pyplot as plt
+from Bio import Phylo
+from Bio.Phylo.TreeConstruction import *
+from Bio.Phylo.TreeConstruction import DistanceTreeConstructor
+
+def vec_dist(a, b):
+    dst = 0
+    for i in range(len(a)):
+        dst += (a[i] - b[i]) ** 2
+    dst = dst ** 0.5
+    return dst
+
+f = open('data2.txt', 'r')
+names = []
+vecs = []
+for line in f:
+    names.append(line.split()[0])
+    vecs.append([float(i) for i in line.split()[1:-1]])
+
+
+dist = [[0] * (i + 1) for i in range(len(vecs))]
+for i in range(len(vecs)):
+    for j in range(i + 1):
+        dist[i][j] = (vec_dist(vecs[i], vecs[j]))
+print(dist)
+dm = DistanceMatrix(names = names, matrix = dist)
+constructor = DistanceTreeConstructor()
+tree = constructor.nj(dm)
+for i in tree.find_clades({"name": "Inner.*"}):
+	i.name = ""
+Phylo.draw(tree, branch_labels=lambda c: str(round(c.branch_length, 2)))
+'''
+net = Phylo.to_networkx(tree)
+networkx.draw(net)
+plt.show()
+'''
